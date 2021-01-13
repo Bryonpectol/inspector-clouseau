1e4081c87f4ac3685a3c8f985465543010a39eb4
Bryon <bryon.pectol@gmail.com>
2021-01-12 19:21:20 -0700

    testing offset

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ let Title = document.getElementById("title");
 
 let Windowie = document.getElementById("windowsize");
 
+let Offset = document.getElementById("windowoffset");
+
 Title.textContent = "Document Title: " + document.title;
 
 h1Main.textContent = "Inspector Clouseau";
@@ -15,5 +17,7 @@ h2Main.textContent = "He Can Do Anything";
 
 Update.textContent = "Page updated at: " + document.lastModified;
 
-Windowie.textContent = "Window Size: Width: " + screen.width + " : Height: " + screen.height;
+Windowie.textContent = "Window Size: Width - " + screen.width + " : Height - " + screen.height;
+
+Offset.textContent = "Window offset: " + window.pageXOffset + window.pageYOffset;
 
