ed271a0ce02882b083cd0796a49ab427bff3b5c0
Bryon <bryon.pectol@gmail.com>
2021-01-12 18:33:49 -0700

    tseting window sizing

--- js/main.js
+++ js/main.js
@@ -2,6 +2,18 @@ let h1Main = document.getElementById("h1Main");
 
 let Update = document.getElementById("update");
 
+let h2Main = document.getElementById("h2Main");
+
+let Title = document.getElementById("title");
+
+let Windowie = document.getElementById("windowsize");
+
+Title.textContent = "Document Title: " + document.title;
+
 h1Main.textContent = "Inspector Clouseau";
+h2Main.textContent = "He Can Do Anything";
 
 Update.textContent = "Page updated at: " + document.lastModified;
+
+Windowie.textContent = "Window Size: Width: " + screen.width + " : Height: " + screen.height;
+
